0076abf103d9a11013ba0d8e37b5842da066b592
dgupta23 <dgupta23@ncsu.edu>
2022-01-31 20:10:30 -0500

    minor changes

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BallScript : MonoBehaviour
 {
+    public float speed = 0;
+   // public TextMeshProUGUI countText;
+   // public GameObject winTextObject;
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+       // winTextObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    // private void OnMove(InputValue movementValue)
+    // {
+    //     Vector2 movementVector = movementValue.Get<Vector2>();
+
+    //     movementX = movementVector.x;
+    //     movementY = movementVector.y;
+    // }
+
+    private void setCountText()
     {
+        //countText.text = "Count: "+ count.ToString();
+        if(count >= 13){
+           // winTextObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other){
+
+        if (other.gameObject.CompareTag("PickUps")) 
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
         
     }
 }
